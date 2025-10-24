8607e5ac88b9fbe9b6586fd4a8c66b333bc2646f
JoaoCarlos22 <jcarlosguimaraes13@gmail.com>
2025-10-24 12:29:28 -0400

    :wrench: chore: adicionando os dados do banco por env

--- db/connection.js
+++ db/connection.js
@@ -1,4 +1,5 @@
 const mysql = require('mysql2');
+require('dotenv').config();
 
 // Cria a conexão com o banco de dados usando variáveis de ambiente
 const pool = mysql.createPool({
@@ -10,5 +11,24 @@ const pool = mysql.createPool({
     connectionLimit: 10,
     queueLimit: 0
 }).promise();
+
+async function testarConexao() {
+    let connection;
+    try {
+        connection = await pool.getConnection();
+        console.log('Conexão com o banco de dados estabelecida com sucesso');
+        console.log('Servidor MySQL conectado');
+    } catch (erro){
+        console.error('ERRO ao conectar com o banco de dados MySQL:');
+        console.error('Detalhes do Erro:', erro.message);
+
+    } finally{
+        if (connection){
+            connection.release();
+        } 
+    }
+}
+
+testarConexao();
 
 module.exports = pool;
